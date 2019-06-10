61e3fc4ab665819f02bd1e3365b457a45088b8c5
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-06-10 16:19:57 -0500

    Modified objects challenge 3

--- assignments/objects.js
+++ assignments/objects.js
@@ -29,7 +29,10 @@ const kennan = {
   "id": 2,
   "name": "Kennan",
   "email": "kdiben1@tinypic.com",
-  "gender": "M"
+  "gender": "M",
+  "speak": function(){
+    return "Hello, my name is Kennan!";
+  }
 }
 console.log(kennan);
 const keven = {
@@ -50,7 +53,7 @@ const antonietta = {
   "id": 5,
   "name": "Antonietta",
   "email": "adaine5@samsung.com",
-  "gender": "F"
+  "gender": "F",
 }
 console.log(antonietta);
 
@@ -75,17 +78,16 @@ console.log(antonietta.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-kennan.speak = "Hello, my name is Kennan!";
-console.log(kennan.speak);
-//is this the right way to do this?? original hint was console.log(kennan.speak());
+console.log(kennan.speak());
+
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-// Let multiplyNums(num1, num2) => {
-//   Return num1*num2;
-// }
-// console.log(antonietta.multiplyNums(3,4));
-//review this part
+antonietta.multiplyNums = function(num1, num2){
+  return num1 * num2;
+}
+console.log(antonietta.multiplyNums(3,4));
+
 
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
